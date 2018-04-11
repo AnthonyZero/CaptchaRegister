560cdda945d6dee00f32ed913dfbf7ecea17ff52
736252868@qq.com <736252868@qq.com>
2018-04-11 11:26:13 +0800

    delete old dir

--- account-aggregator/account-email/src/main/java/com/pingjin/mvnbook/account/email/AccountEmailService.java
+++
@@ -1,6 +0,0 @@
-package com.pingjin.mvnbook.account.email;
-
-public interface AccountEmailService {
-	//收件地址，主题，内容
-	void sendMail(String to,String subject,String htmlText) throws AccountEmailException;
-}
